6b672b33bac155883a5330d550251bd24b1d2749
Son-Tr <sontran.kh1502@gmail.com>
2024-09-21 23:20:58 +0700

    fist commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import { Route, Routes } from 'react-router-dom';
+import './styles/main.scss';
+import Home from './pages/Home';
+import Layout from './layout/layout';
+import About from './pages/About';
+import Contact from './pages/Contact';
+import Services from './pages/Services';
+import ErrorPage from './pages/ErrorPage';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Routes>
+        <Route path='/' element={<Layout Component={Home} />} />
+        <Route path='/about' element={<Layout Component={About} />} />
+        <Route path='/contact' element={<Layout Component={Contact} />} />
+        <Route path='/services' element={<Layout Component={Services} />} />
+        <Route path='*' element={<Layout Component={ErrorPage} />} />
+
+      </Routes>
     </div>
   );
 }
---
+++ src/layout/layout.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Layout({ Component }) {
+    return (
+        <>
+            Layout
+            <Component />
+            Layout
+        </>
+    )
+}
+
+export default Layout
