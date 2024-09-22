6b7803d662667b6e9a0e157db5c64b2725b16530
Son-Tr <sontran.kh1502@gmail.com>
2024-09-23 00:33:50 +0700

    handle header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,33 +1,40 @@
 import { faBars, faBarsStaggered } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import React, { useState } from "react";
-import { Link } from "react-router-dom";
+import React, { useEffect, useState } from "react";
+import { NavLink, useLocation } from "react-router-dom";
 import "./Header.scss"
 
 
 
 function Header() {
-    const [isMenu, setIsMenu]= useState(false);
-    const toggleMenu = ()=>{
+    const [isMenu, setIsMenu] = useState(false);
+    const location = useLocation(); // Hook to get the current location (route)
+
+    // Close the menu whenever the route changes
+    useEffect(() => {
+        setIsMenu(false);
+    }, [location]); // Trigger the effect when the location (route) changes
+
+    const toggleMenu = () => {
         setIsMenu(!isMenu)
     }
     return (
         <div>
-        <header className="container">
-            <Link to="/">
-                <span className="icon-logo"><FontAwesomeIcon icon={faBarsStaggered} /></span>
-                <span className="logo">Corp Vision</span>
-            </Link>
-            <nav className={isMenu?"show":""}>
-                <Link className="nav-link active" to='/'>Home</Link>
-                <Link className="nav-link" to='/about'>About</Link>
-                <Link className="nav-link" to='/services'>Services</Link>
-                <Link className="nav-link" to='/Contact'>Contact</Link>
-            </nav>
-            <span className="bars-icon"  onClick={toggleMenu}>
-            <FontAwesomeIcon icon={faBars} />
-            </span>
-        </header>
+            <header className="container">
+                <NavLink to="/">
+                    <span className="icon-logo"><FontAwesomeIcon icon={faBarsStaggered} /></span>
+                    <span className="logo">Corp Vision</span>
+                </NavLink>
+                <nav className={isMenu ? "show" : ""}>
+                    <NavLink className="nav-link" to='/'>Home</NavLink>
+                    <NavLink className="nav-link" to='/about'>About</NavLink>
+                    <NavLink className="nav-link" to='/services'>Services</NavLink>
+                    <NavLink className="nav-link" to='/Contact'>Contact</NavLink>
+                </nav>
+                <span className="bars-icon" onClick={toggleMenu}>
+                    <FontAwesomeIcon icon={faBars} />
+                </span>
+            </header>
         </div>
     )
 }
