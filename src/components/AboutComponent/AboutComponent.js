f354190d26cb196310df37d62fb33628d3f72237
Son-Tr <sontran.kh1502@gmail.com>
2024-09-26 12:03:08 +0700

    upadte mixin

---
+++ src/components/AboutComponent/AboutComponent.js
@@ -0,0 +1,25 @@
+import { div } from "framer-motion/client";
+import React from "react";
+
+function AboutComponent() {
+    return (
+        <div className="container">
+            <div className="about-img">
+
+            </div>
+            <div className="about-content">
+                <h5 className="title-small">
+                    About our company
+                </h5>
+                <h2 className="title-big">
+                    We are the next generation of the business world, creative solutions
+                </h2>
+                <h4 className="h4-content">
+                    Over 25 years, We complete every project with extra care as customer needs.
+                </h4>
+            </div>
+        </div>
+    )
+}
+
+export default AboutComponent;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import Banner from '../components/Banner/Banner'
+import AboutComponent from '../components/AboutComponent/AboutComponent'
 
 const Home = () => {
   return (
     <div className=''>
       <Banner/>
+      <AboutComponent/>
     </div>
   )
 }
