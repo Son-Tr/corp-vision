26c9209e5293ec8a63a9c627d49b01fdd81b1077
Son-Tr <sontran.kh1502@gmail.com>
2024-09-27 21:03:33 +0700

    handle about

--- src/components/AboutComponent/AboutComponent.js
+++ src/components/AboutComponent/AboutComponent.js
@@ -1,22 +1,81 @@
-import { div } from "framer-motion/client";
+
 import React from "react";
+import { about2 } from "../../assets/img/img";
+import "./AboutComponent.scss";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faLightbulb, faObjectGroup, faThumbsUp } from "@fortawesome/free-regular-svg-icons";
 
 function AboutComponent() {
     return (
-        <div className="container">
-            <div className="about-img">
-
-            </div>
-            <div className="about-content">
-                <h5 className="title-small">
-                    About our company
-                </h5>
-                <h2 className="title-big">
-                    We are the next generation of the business world, creative solutions
-                </h2>
-                <h4 className="h4-content">
-                    Over 25 years, We complete every project with extra care as customer needs.
-                </h4>
+        <div id="about">
+            <div className="container">
+                <div className="flex-about">
+                    <div className="about-img">
+                        <img src={about2} alt="about2" />
+                    </div>
+                    <div className="about-content">
+                        <h5 className="title-small">
+                            About our company
+                        </h5>
+                        <h2 className="title-big">
+                            We are the next generation of the business world, creative solutions
+                        </h2>
+                        <h4 className="h4-content">
+                            Over 25 years, We complete every project with extra care as customer needs.
+                        </h4>
+                        <p className="p-content">
+                            We are the next generation of the business world, bringing creative solutions. With over 25 years of experience, we handle each project with the utmost care, ensuring that every customer’s needs are met to the highest standards. Our dedicated team continually strives for excellence, delivering innovative results in a fast-changing marketplace. We prioritize quality, integrity, and long-term success.
+                        </p>
+                        <button className="btn">Read More</button>
+                    </div>
+                </div>
+                <div className="list-feature">
+                    <div className="box">
+                        <div className="box-content">
+                            <div className="icon-left">
+                            <FontAwesomeIcon icon={faObjectGroup} />
+                            </div>
+                            <div className="content-right">
+                                <p className="p-title">
+                                    Perfect Design
+                                </p>
+                                <p className="p-content">
+                                Crafted with precision, every element falls into place effortlessly.
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="box">
+                        <div className="box-content">
+                            <div className="icon-left">
+                            <FontAwesomeIcon icon={faThumbsUp} />
+                            </div>
+                            <div className="content-right">
+                                <p className="p-title">
+                                    Carefully Planned
+                                </p>
+                                <p className="p-content">
+                                Every detail is thoughtfully considered, ensuring a solid foundation for success.
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="box">
+                        <div className="box-content">
+                            <div className="icon-left">
+                            <FontAwesomeIcon icon={faLightbulb} />
+                            </div>
+                            <div className="content-right">
+                                <p className="p-title">
+                                    Smartly Execute
+                                </p>
+                                <p className="p-content">
+                                Actions are taken with precision, transforming plans into effective results.
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     )
