6e4be21ab5a4ceb9273a1209e71b70dfda63426a
Son-Tr <sontran.kh1502@gmail.com>
2024-09-27 22:33:33 +0700

    upadte event scroll

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,8 +1,9 @@
-import { faBars, faBarsStaggered } from "@fortawesome/free-solid-svg-icons";
+
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import React, { useEffect, useState } from "react";
 import { NavLink, useLocation } from "react-router-dom";
 import "./Header.scss"
+import { faBars, faBarsStaggered, faMoon } from "@fortawesome/free-solid-svg-icons";
 
 
 
@@ -14,6 +15,20 @@ function Header() {
     useEffect(() => {
         setIsMenu(false);
     }, [location]); // Trigger the effect when the location (route) changes
+
+    useEffect(() => {
+        let navBar = document.querySelector(".nav-header")
+        const handleScroll = () => {
+            navBar.classList.toggle("nav-header-fixed",window.scrollY > 80); // add  nav-header-fixed class when window.scrollY > 80
+        }
+        
+        window.addEventListener("scroll", handleScroll);
+
+        // Cleanup function to remove the event listener
+        return () => {
+            window.removeEventListener("scroll", handleScroll);
+        }
+    }, [window.scrollY])
 
     const toggleMenu = () => {
         setIsMenu(!isMenu)
@@ -30,6 +45,9 @@ function Header() {
                     <NavLink className="nav-link" to='/about'>About</NavLink>
                     <NavLink className="nav-link" to='/services'>Services</NavLink>
                     <NavLink className="nav-link" to='/Contact'>Contact</NavLink>
+                    <span className="light-dark-btn">
+                        <FontAwesomeIcon icon={faMoon} />
+                    </span>
                 </nav>
                 <span className="bars-icon" onClick={toggleMenu}>
                     <FontAwesomeIcon icon={faBars} />
