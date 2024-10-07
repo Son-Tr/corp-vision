c6fe1771c257357596c51da897c9a091f2192cae
Son-Tr <sontran.kh1502@gmail.com>
2024-10-07 20:10:00 +0700

    complete back to top button

--- src/components/BackToTop/BackToTop.js
+++ src/components/BackToTop/BackToTop.js
@@ -1,10 +1,48 @@
-import React from 'react'
 import "./BackToTop.scss"
+import React, { useEffect, useState } from 'react'
+import { motion, AnimatePresence } from 'framer-motion';
+import { faArrowUp } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
 const BackToTop = () => {
-  return (
-    <div>BackToTop</div>
-  )
+    const [visible, setVisible] = useState(false);
+
+    // handle event scrollY to display back to top btn
+    useEffect(() => {
+        const visibleDisplay = () => {
+            setVisible(window.scrollY > 500)// check true or false for visible
+        }
+
+        window.addEventListener("scroll", visibleDisplay);
+
+        return () => {
+            window.removeEventListener("scroll", visibleDisplay);
+        }
+    }, [])
+
+    // function back to top
+    const backToTop = () => {
+        window.scrollTo(0, 0)
+    }
+
+    return (
+        <>
+            <AnimatePresence>
+                {visible &&
+                    <motion.span onClick={backToTop} className='back-to-top'
+                        initial={{ opacity: 0, scale: 0.3 }}
+                        animate={{ opacity: 1, scale: 1 }}
+                        exit={{ opacity: 0, scale: 0.3 }}
+                        transition={{
+                            duration: 0.3   ,
+                             ease: "easeInOut",
+                        }}
+                    >
+                        <FontAwesomeIcon icon={faArrowUp} />
+                    </motion.span>}
+            </AnimatePresence>
+        </>
+    )
 }
 
 export default BackToTop
--- src/layout/layout.js
+++ src/layout/layout.js
@@ -1,6 +1,7 @@
 import React from "react";
 import Header from "../components/Header/Header";
 import Footer from "../components/Footer/Footer";
+import BackToTop from "../components/BackToTop/BackToTop";
 
 function Layout({ Component }) {
     return (
@@ -8,6 +9,7 @@ function Layout({ Component }) {
             <Header/>
             <Component  />
             <Footer/>
+            <BackToTop/>
         </>
     )
 }
